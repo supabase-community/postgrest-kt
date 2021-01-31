8aa25c2e1cfc9d8c133fec62d1a3f1a1d8fb7bd0
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-01-31 20:49:03 +0100

    Tests for extracting count

--- src/main/kotlin/io/supabase/postgrest/http/ExtractCount.kt
+++ src/main/kotlin/io/supabase/postgrest/http/ExtractCount.kt
@@ -1,12 +1,15 @@
 package io.supabase.postgrest.http
 
+/**
+ * PostgREST provides the count in the content-range header, if a prefer header is sent requesting the count information.
+ */
 fun extractCount(responseHeaders: Map<String, String>, requestHeaders: Map<String, String>): Long? {
-    val preferHeader = requestHeaders["Prefer"]
+    val preferHeader = requestHeaders.entries.firstOrNull { it.key.toLowerCase() == "prefer" }?.value
 
     val countHeader = preferHeader != null && preferHeader.matches(Regex("count=(exact|planned|estimated)"))
 
     return if (countHeader) {
-        val contentRange = responseHeaders["content-range"]?.split("/")
+        val contentRange = responseHeaders.entries.firstOrNull { it.key.toLowerCase() == "content-range" }?.value?.split("/")
         if (contentRange != null && contentRange.size > 1) {
             contentRange[1].toLong()
         } else {
