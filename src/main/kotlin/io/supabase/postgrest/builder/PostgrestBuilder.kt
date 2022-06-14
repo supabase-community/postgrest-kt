9d75db5b562c96f534d55bac557e50633239afaf
Ethon <48890023@qq.com>
2022-06-14 19:07:54 +0800

    fix: select params in QueryBuilder should set the FilterBuilder (#7)

--- src/main/kotlin/io/supabase/postgrest/builder/PostgrestBuilder.kt
+++ src/main/kotlin/io/supabase/postgrest/builder/PostgrestBuilder.kt
@@ -30,6 +30,10 @@ open class PostgrestBuilder<T : Any> {
         this.body = builder.body
         this.jsonConverter = builder.jsonConverter
         this.schema = schema
+
+        builder.searchParams.forEach {
+            (name, value) -> setSearchParam(name, value)
+        }
     }
 
     constructor(url: URI, httpClient: PostgrestHttpClient, jsonConverter: PostgrestJsonConverter, headers: Map<String, String>, schema: String?) {
