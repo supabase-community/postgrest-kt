764872e519236f1e6718853df5c5e4103a3434cb
Samuele Bruschi <samuele.bruschi@abstract.it>
2022-07-12 16:22:09 +0200

    Add Constructor header as map

--- src/commonMain/kotlin/io/supabase/postgrest/PostgrestDefaultClient.kt
+++ src/commonMain/kotlin/io/supabase/postgrest/PostgrestDefaultClient.kt
@@ -1,7 +1,6 @@
 package io.supabase.postgrest
 
 import io.ktor.client.*
-import io.ktor.client.engine.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
@@ -13,20 +12,7 @@ class PostgrestDefaultClient(
     uri: Url,
     headers: Headers = headersOf(),
     schema: String? = null,
-    clientEngine: HttpClientEngine,
     json: Json = Json {
-//        serializersModule = SerializersModule {
-//            contextual(String.serializer())
-//            contextual(Int.serializer())
-//            contextual(Float.serializer())
-//            contextual(Double.serializer())
-//            contextual(Boolean.serializer())
-//            contextual(Char.serializer())
-//            contextual(Byte.serializer())
-//            contextual(PolymorphicSerializer(Map::class))
-//
-//        }
-        this.allowStructuredMapKeys
         ignoreUnknownKeys = true
     }
 ) : PostgrestClient(
@@ -35,7 +21,7 @@ class PostgrestDefaultClient(
     schema = schema,
     httpClient = {
         PostgrestHttpClient(
-            HttpClient(clientEngine) {
+            HttpClient {
                 install(Logging) {
 
                 }
@@ -46,4 +32,24 @@ class PostgrestDefaultClient(
             }
         )
     }
-)
+) {
+
+    constructor(
+        uri: Url,
+        headersMap: Map<String, String> = mapOf(),
+        schema: String? = null,
+        json: Json = Json {
+            ignoreUnknownKeys = true
+        }
+    ) : this(
+        uri,
+        Headers.build {
+            headersMap.entries.forEach {
+                append(it.key, it.value)
+            }
+        },
+        schema,
+        json
+    )
+
+}
